a3587b0c780e44978e1c771db767979fc19ffa96
batoul351 <btwlalywns351@gmail.com>
2025-09-18 22:52:21 +0300

    استخدام ال dio بدل ال http واستخدام الgetstorage shared_preferencesبدل ال

--- lib/api_service/api_sign_up_service.dart
+++ lib/api_service/api_sign_up_service.dart
@@ -1,18 +1,23 @@
 import 'dart:convert';
 import 'package:get/get.dart';
-import 'package:http/http.dart' as http;
+import 'package:dio/dio.dart';
+import 'package:get_storage/get_storage.dart';
 import '../screen/signup.dart';
 import '../screen/VerifyAccountPage .dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'api_Parts.dart';
+
 class SignUpController extends GetxController {
   static const String baseUrl = "http://192.168.1.103:8000/api";
+  final Dio dio = Dio();
+  final GetStorage storage = GetStorage();
+
   var isLoading = false.obs;
   var signUpMessage = "".obs;
-  Future<void> saveToken(String token) async {
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.setString('access_token', token);
+
+  void saveToken(String token) {
+    storage.write('access_token', token);
   }
+
   Future<void> signUp({
     required String firstName,
     required String lastName,
@@ -20,38 +25,46 @@ class SignUpController extends GetxController {
     required String password,
   }) async {
     const String endpoint = "/signup";
-    final url = Uri.parse(baseUrl + endpoint);
+    final String url = baseUrl + endpoint;
+
     try {
       isLoading.value = true;
-      final response = await http.post(
+
+      final response = await dio.post(
         url,
-        headers: {
-          "Content-Type": "application/json",
-          "Accept": "application/json",
-        },
-        body: jsonEncode({
+        options: Options(
+          headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+          },
+        ),
+        data: {
           "first_name": firstName,
           "last_name": lastName,
           "email": email,
           "password": password,
-        }),
+        },
       );
+
       isLoading.value = false;
+
       if (response.statusCode == 201) {
-        final responseData = jsonDecode(response.body);
+        final responseData = response.data;
         if (responseData.containsKey("token")) {
           String token = responseData["token"];
-          await saveToken(token); 
+          saveToken(token);
           signUpMessage.value = "تم تسجيل المستخدم بنجاح!";
-          Get.snackbar("نجاح", signUpMessage.value, backgroundColor: Get.theme.primaryColor); 
+          Get.snackbar("نجاح", signUpMessage.value, backgroundColor: Get.theme.primaryColor);
+
           final CategoryController categoryController = Get.put(CategoryController());
           categoryController.fetchCategories();
+
           Get.to(() => VerifyAccountPage(email: email));
         } else {
-          signUpMessage.value = jsonDecode(response.body)['message'] ?? "خطأ غير معروف";
+          signUpMessage.value = responseData['message'] ?? "خطأ غير معروف";
         }
       } else {
-        signUpMessage.value = jsonDecode(response.body)['message'] ?? "خطأ غير معروف";
+        signUpMessage.value = response.data['message'] ?? "خطأ غير معروف";
       }
     } catch (error) {
       isLoading.value = false;
--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'api_service/notification.dart'; // هذا هو FCMService
+import 'package:get_storage/get_storage.dart';
+import 'api_service/notification.dart'; // FCMService
 import 'screen/home_page.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize Firebase
   await Firebase.initializeApp();
 
-  final prefs = await SharedPreferences.getInstance();
-  final userToken = prefs.getString('userToken');
+  // Initialize GetStorage
+  await GetStorage.init();
+  final storage = GetStorage();
+
+  // Read user token from storage
+  final userToken = storage.read('userToken');
 
   if (userToken != null) {
     await FCMService.initialize(userToken);
   } else {
-    print(" لا يوجد توكن مستخدم محفوظ");
+    print("لا يوجد توكن مستخدم محفوظ");
   }
 
   runApp(MyApp());
